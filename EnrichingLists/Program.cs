b6db986a62f4353bd2b7eae087aae418ddad01ff
Каюн Максим <maks.kayun@yandex.ru>
2023-12-16 19:07:21 +0300

    add files

--- EnrichingLists/Program.cs
+++ EnrichingLists/Program.cs
@@ -1,3 +1,67 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using ClosedXML.Excel;
+
+namespace Enriching_lists
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var tableMain = GetTimeTable("Введите путь до файла с данными (основной) >");
+            var tableInstr = GetTimeTableDirectory("Введите путь до ДИРЕКТОРИИ с файлами с данными (инструкторские) >");
+            
+            FindAndSetDescription(tableMain, tableInstr);
+            
+            tableMain.UpdateData();
+        }
+
+        internal static Timetable GetTimeTable(string message)
+        {
+            Console.Write(message);
+            var path = Console.ReadLine()?.TrimStart('\"').TrimEnd('\"');
+            var timetable = new Timetable(path);
+            timetable.LoadData();
+            return timetable;
+        }
+
+        internal static void FindAndSetDescription(Timetable mainTable, List<Timetable> secondaryTables)
+        {
+            foreach (var secondaryTable in secondaryTables)
+            {
+                for (var i = 0; i < secondaryTable.Items.Count; i++)
+                {
+                    var key = secondaryTable.Items[i].Key;
+                    var record = mainTable.Items
+                        .FirstOrDefault(e => e.Key == key)?
+                        .Description ?? default;
+
+                    if (string.IsNullOrWhiteSpace(mainTable.Items[i].Description) && record != default &&
+                        !string.IsNullOrWhiteSpace(record))
+                    {
+                        mainTable.Items[i].Description = record;
+                    }
+                }
+            }
+        }
+
+        internal static List<Timetable> GetTimeTableDirectory(string message)
+        {
+            Console.Write(message);
+            var pathDir = Console.ReadLine()?.TrimStart('\"').TrimEnd('\"');
+            var files = Directory.GetFiles(pathDir);
+
+            var result = new List<Timetable>();
+
+            foreach (var path in files)
+            {
+                var timetable = new Timetable(path);
+                timetable.LoadData();
+
+                result.Add(timetable);
+            }
+
+            return result;
+        }
+    }
+}
--- EnrichingLists/Timetable.cs
+++ EnrichingLists/Timetable.cs
@@ -1,6 +1,75 @@
-﻿namespace Enriching_lists;
+﻿using ClosedXML.Excel;
+
+namespace Enriching_lists;
 
 public class Timetable
 {
-    
+    public class RecordObject
+    {
+        public string NumberTrain { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Route { get; set; }
+        public string Cause { get; set; }
+
+        public string Description { get; set; }
+
+        public string Key => $"{NumberTrain}_{DateTime}_{Route}_{Cause}";
+    }
+    public List<RecordObject> Items { get; set; } = new();
+
+    public Timetable(string path)
+    {
+        this.path = path;
+    }
+
+    public void LoadData()
+    {
+        using var wbook = new XLWorkbook(path);
+        var sheet = wbook.Worksheet("Лист1");
+
+        for (var i = 3; i <= sheet.Rows().Count(); i++)
+        {
+            if (sheet.Row(i).IsHidden)
+            {
+                continue;
+            }
+
+            var row = sheet.Row(i);
+            var obj = new RecordObject
+            {
+                NumberTrain = row.Cell(1).GetValue<string>(),
+                DateTime = DateTime.Parse(row.Cell(2).GetValue<string>()),
+                Route = row.Cell(3).GetValue<string>(),
+                Cause = row.Cell(4).GetValue<string>(),
+                Description = row.Cell(11).GetValue<string>()
+            };
+            Items.Add(obj);
+        }
+    }
+
+    public void UpdateData()
+    {
+        using var wbook = new XLWorkbook(path);
+        var sheet = wbook.Worksheet("Лист1");
+
+        for (var i = 3; i <= sheet.Rows().Count(); i++)
+        {
+            if (sheet.Row(i).IsHidden)
+            {
+                continue;
+            }
+
+            var raw = sheet.Row(i);
+            var key =
+                $"{raw.Cell(1).GetValue<string>()}_" +
+                $"{DateTime.Parse(raw.Cell(2).GetValue<string>())}_" +
+                $"{raw.Cell(3).GetValue<string>()}_" +
+                $"{raw.Cell(4).GetValue<string>()}";
+            sheet.Cell(i, 11).Value = Items.FirstOrDefault(e => e.Key == key)?.Description;
+        }
+        
+        wbook.Save();
+    }
+
+    private string path { get; set; }
 }
